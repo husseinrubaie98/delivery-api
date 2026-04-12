683be22f42ef5994881ac9c4a39139eb2dae8708
husseinrubaie98 <77251920+husseinrubaie98@users.noreply.github.com>
2026-04-12 03:18:45 +0300

    Finish Section 1: Create simple controller; set app properties

---
+++ src/main/java/com/husseinrubaie/delivery/rest/FunRestController.java
@@ -0,0 +1,26 @@
+package com.husseinrubaie.delivery.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${info.author.firstname}")
+    public String firstname;
+
+    @Value("${info.author.lastname}")
+    public String lastname;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "<h1>Hello World!</h1><br>From <b> " + firstname + " " + lastname + "</b>";
+
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "<h1>Run a hard 15k!!</h1>";
+    }
+}
