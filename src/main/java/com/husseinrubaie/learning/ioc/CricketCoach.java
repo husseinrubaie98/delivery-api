bda72c81701e1b93e1f457f75e885d9071459cda
husseinrubaie98 <77251920+husseinrubaie98@users.noreply.github.com>
2026-07-18 21:42:57 +0300

    lazy loading

--- src/main/java/com/husseinrubaie/learning/ioc/CricketCoach.java
+++ src/main/java/com/husseinrubaie/learning/ioc/CricketCoach.java
@@ -1,10 +1,12 @@
 package com.husseinrubaie.learning.ioc;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Lazy // Bean is only initialized when needed for dependency injection.
 public class CricketCoach implements Coach {
 
     @Override
